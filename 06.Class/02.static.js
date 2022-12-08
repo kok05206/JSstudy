16fa2ba9af46262689a679c2a1a35568cd23ae0a
Inhwan Hwang <ghkddlsghks37@gmail.com>
2022-12-08 16:19:43 +0900

    Chapter06_02.static.js
    Javascript-Class
    learn - static

---
+++ 06.Class/02.static.js
@@ -0,0 +1,35 @@
+// static 정적 프로퍼티, 메서드
+class Fruit {
+    static MAX_FRUIT = 4;
+    // 생성자: new 키워드로 객체를 생성할 떄 호출되는 함수
+    constructor(name, emoji) {
+            this.name = name;
+            this.emoji = emoji;
+        }
+        // 클래스 레벨의 메서드
+    static makeRandomFruit() {
+            // 클래스 레벨의 메서드에서는 this를 참조할 수 없음
+            return new Fruit('banana', '🍌');
+        }
+        // 인스턴스 레벨의 메서드
+    display = () => {
+        console.log(`${this.name}: ${this.emoji}`);
+    };
+}
+
+const banana = Fruit.makeRandomFruit();
+console.log(banana);
+console.log(Fruit.MAX_FRUIT);
+// apple은 Fruit 클래스의 인스턴스이다.
+const apple = new Fruit('apple', '🍎');
+// orange는 Fruit 클래스의 인스턴스이다.
+const orange = new Fruit('orange', '🍊');
+
+console.log(apple);
+console.log(orange);
+
+console.log(apple.name);
+console.log(orange.name);
+
+apple.display();
+orange.display();
