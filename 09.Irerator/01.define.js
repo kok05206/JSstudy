513687e287c2a1be524888f20cd13637c86ff382
Inhwan Hwang <ghkddlsghks37@gmail.com>
2023-02-06 14:59:32 +0900

    Chapter09.Iterator(01_define)
    Iterator의 개념정리 및 Protocol 정의와 Iteration Protocol, Iteration Protocol, DataStructure과 기본 동작 흐름정리.

---
+++ 09.Irerator/01.define.js
@@ -0,0 +1,19 @@
+/**
+ * Iteration
+ * 반복, 순회
+ * Protocol
+ * 규격, 약속, 인터페이스
+ * Iteration Protocol을 따른다? -> 반복, 순회가 가능하다.
+ * Iteration Protocol을 따르는 객체
+ * 1. for...of
+ * 2. spread
+ * 자료구조
+ * 1. Array
+ * 2. String
+ * 3. Map
+ * 4. Set
+ * Iterable 프로토콜을 따른다면
+ * [Symbol.iterator](): 을 호출
+ * Iterator 프로토콜을 따르는 객체를 리턴
+ * Iterator 프로토콜은 next():를 가지고 있기에 다음값을 리턴해주도록 해야 함.
+ */
