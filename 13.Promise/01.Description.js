6e7ebbe35ecc8a80757581c16545cb033b55d9ad
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2023-02-17 16:01:58 +0900

    Chapter13.Promise(01.description.js)
    비동기에 대해서 배우기 전 간단한 설명을 들었음.

---
+++ 13.Promise/01.Description.js
@@ -0,0 +1,11 @@
+/**
+ * JavaScript Runtime Ecvironment
+ * ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+ * JavaScript Engine
+ * ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+ * 소스코드에 동적으로 객체를 생성하는 경우에는 -> Memory Heap에 생성됨.
+ * Call Stack : 함수의 실행순서를 기억한다.
+ * JavaScript Engine은 하나의 실글 컨텍스트 스택을 가지고 있다.
+ * 한 번에 하나의 일만 처리할 수 있다. -> Single Thread
+ * 자바스크립트는 기본적으로 동기적으로 진행.
+ */
