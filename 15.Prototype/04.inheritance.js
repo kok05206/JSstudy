eec7281c8dca1f33a1723b7843cd50959ae6f4b8
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2023-02-25 13:24:13 +0900

    Chapter15.Prototype(04.inheritance.js)
    본격적인 프로토타입을 사용해서 함수나 오브젝트에 직접 적용시켜 실습을 진행해보았고, 상속도 확인하는 방법까지 실습해보았다.

---
+++ 15.Prototype/04.inheritance.js
@@ -0,0 +1,43 @@
+// 프로토타일을 베이스로한 객체지향 프로그래밍
+function Animal(name, emoji) {
+  this.name = name;
+  this.emoji = emoji;
+}
+
+Animal.prototype.printName = function () {
+  console.log(`${this.name} ${this.emoji}`);
+};
+
+function Dog(name, emoji, owner) {
+  // super(name, emoji)
+  Animal.call(this, name, emoji);
+}
+// Dog.prototype = Object.create(Object.prototype)
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.play = () => {
+  console.log('같이 놀자옹!');
+};
+
+function Tiger(name, emoji) {
+  Animal.call(this, name, emoji);
+}
+Tiger.prototype = Object.create(Animal.prototype);
+Tiger.prototype.hunt = () => {
+  console.log('사냥하자! ..🐇..');
+};
+const dog1 = new Dog('멍멍', '🐶', '인환');
+dog1.play();
+dog1.printName();
+
+const tiger1 = new Tiger('어흥', '🐯');
+tiger1.printName();
+tiger1.hunt();
+
+// 상속도 확인법!
+console.log(dog1 instanceof Dog);
+console.log(dog1 instanceof Animal);
+console.log(dog1 instanceof Tiger);
+
+console.log(tiger1 instanceof Dog);
+console.log(tiger1 instanceof Animal);
+console.log(tiger1 instanceof Tiger);
